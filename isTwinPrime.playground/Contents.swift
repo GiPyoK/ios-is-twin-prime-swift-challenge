9a6420c06f4848341799faa52618ac8f625fe567
Gi Pyo Kim <gipyok@gmail.com>
2019-11-06 08:37:18 -0800

    completed

--- isTwinPrime.playground/Contents.swift
+++ isTwinPrime.playground/Contents.swift
@@ -1,6 +1,26 @@
 import UIKit
 
+func isPrime(_ number: Int) -> Bool {
+    if number <= 1 { return false }
+    
+    let flag = true
+    for i in 2..<number {
+        if (number % i) == 0 {
+            return false
+        }
+    }
+    return flag
+}
 
+func isTwinPrime(_ number: Int) -> Bool {
+    if !isPrime(number) { return false }
+    
+    if isPrime(number - 2) || isPrime(number + 2) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -17,14 +37,14 @@ import UIKit
 
 
 //Test cases
-//isTwinPrime(2)   // false
-//isTwinPrime(10)  // false
-//isTwinPrime(9)   // false
-//isTwinPrime(4)   // false
-//isTwinPrime(5)   // true
-//isTwinPrime(7)   // true
-//isTwinPrime(23)  // false
-//isTwinPrime(37)  // false
-//isTwinPrime(883) // true
-//isTwinPrime(907) // false
-//isTwinPrime(169) // false
+isTwinPrime(2)   // false
+isTwinPrime(10)  // false
+isTwinPrime(9)   // false
+isTwinPrime(4)   // false
+isTwinPrime(5)   // true
+isTwinPrime(7)   // true
+isTwinPrime(23)  // false
+isTwinPrime(37)  // false
+isTwinPrime(883) // true
+isTwinPrime(907) // false
+isTwinPrime(169) // false
